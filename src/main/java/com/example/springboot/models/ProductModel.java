52f50acc5edd51f8fd827d9935b5630368bc6837
Derick <derick1castro@gmail.com>
2023-08-30 16:54:16 -0300

    Model and entity product

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -12,7 +12,7 @@ public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idProduct;
     private String name;
     private BigDecimal value;
