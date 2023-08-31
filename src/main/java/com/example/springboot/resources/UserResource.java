fa08c6bab62fdab23d5c6e60d983a0481fddc894
Derick <derick1castro@gmail.com>
2023-08-31 15:03:01 -0300

    Implemeting user entity

--- src/main/java/com/example/springboot/resources/UserResource.java
+++ src/main/java/com/example/springboot/resources/UserResource.java
@@ -10,13 +10,16 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(value = "/users")
 public class UserResource {
 
     @Autowired
@@ -27,5 +30,31 @@ public class UserResource {
         UserModel userModel = new UserModel();
         BeanUtils.copyProperties(userRecordDto, userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.insert(userModel));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers(){
+        List<UserModel> usersList = userService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(usersList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneUser(@PathVariable(value="id") UUID id) {
+        UserModel userOptional = userService.findById(id);
+        if (userOptional == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        //productOptional.add(linkTo(methodOn(ProductResource.class).getAllProducts()).withRel("Products List"));
+        return ResponseEntity.status(HttpStatus.OK).body(userOptional);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") UUID id) {
+        UserModel userOptional = userService.findById(id);
+        if(userOptional == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+        userService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body("User deleted successfully.");
     }
 }
--- src/main/java/com/example/springboot/services/UserService.java
+++ src/main/java/com/example/springboot/services/UserService.java
@@ -1,9 +1,14 @@
 package com.example.springboot.services;
 
+import com.example.springboot.models.ProductModel;
 import com.example.springboot.models.UserModel;
 import com.example.springboot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -11,7 +16,29 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    //inserir todos os usuários
     public UserModel insert(UserModel userModel){
         return userRepository.save(userModel);
+    }
+
+    //Listar todos os usuários
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    //Listar um único usuário
+    public UserModel findById(UUID id) {
+        Optional<UserModel> userOptional = userRepository.findById(id);
+        return userOptional.get();
+    }
+
+    //Deletar um usuário
+    public void delete(UUID id){
+        try{
+            userRepository.deleteById(id);
+        }catch (RuntimeException e){
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
