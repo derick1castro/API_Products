26f40ad2a894226d08ba6a0a1f572852130094d7
Derick <derick1castro@gmail.com>
2023-08-31 13:23:28 -0300

    Creating package Service and Resource

--- src/main/java/com/example/springboot/repositories/ProductRepository.java
+++ src/main/java/com/example/springboot/repositories/ProductRepository.java
@@ -1,11 +1,15 @@
 package com.example.springboot.repositories;
 
 import com.example.springboot.models.ProductModel;
+import jakarta.persistence.metamodel.SingularAttribute;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
 }
---
+++ src/main/java/com/example/springboot/services/ProductService.java
@@ -0,0 +1,55 @@
+package com.example.springboot.services;
+
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public ProductModel insert(ProductModel productModel){
+        return productRepository.save(productModel);
+    }
+
+    public List<ProductModel> findAll(){
+        return productRepository.findAll();
+    }
+
+    public ProductModel findById(UUID id){
+        Optional<ProductModel> productO = productRepository.findById(id);
+        return productO.get();
+    }
+
+    public void delete(UUID id){
+        try {
+            productRepository.deleteById(id);
+        } catch (RuntimeException e){
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    public ProductModel update(UUID id, ProductModel productModel) {
+        try {
+            ProductModel productO = productRepository.getReferenceById(id);
+            updateData(productO, productModel);
+            return productRepository.save(productO);
+        } catch (RuntimeException e) {
+             e.printStackTrace();
+             throw e;
+        }
+    }
+
+    private void updateData(ProductModel productO, ProductModel productModel) {
+        productO.setName(productModel.getName());
+        productO.setValue(productModel.getValue());
+    }
+}
